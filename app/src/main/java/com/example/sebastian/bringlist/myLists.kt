8de6c0c8f90591f8e255468b02e1b4915938fc6e
Juan Sebastian Rivera <juansebastianrivera@hotmail.com>
2018-04-03 15:53:17 -0500

    Added connection to Firebase databases to extract the name
    Added function to get the name from Firebase and display it on the name displayer over the Menu.

--- app/src/main/java/com/example/sebastian/bringlist/myLists.kt
+++ app/src/main/java/com/example/sebastian/bringlist/myLists.kt
@@ -9,15 +9,38 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_my_lists.*
 import kotlinx.android.synthetic.main.app_bar_my_lists.*
+import kotlinx.android.synthetic.main.nav_header_my_lists.*
 
 class myLists : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private var mDatabase: DatabaseReference? = null
+    private var mMessageReference: DatabaseReference? = null
+    private var mAuth: FirebaseAuth? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_lists)
         setSupportActionBar(toolbar)
+        mDatabase = FirebaseDatabase.getInstance().reference
+        mMessageReference = FirebaseDatabase.getInstance().getReference("Users")
+        mAuth = FirebaseAuth.getInstance()
+        val userId = mAuth!!.currentUser!!.uid
+        mDatabase!!.child("Users").child(userId).child("name").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError?) {
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val name = dataSnapshot.getValue(String::class.java)
+                tvName.text = name
+            }
+        })
+
+
 
 
         val toggle = ActionBarDrawerToggle(
